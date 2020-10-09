734f05c3f291f22cb65e7011ef060cd0f3b6dccf
Mateus Nogueira <mateus_nogol@outlook.com>
2020-10-09 20:29:43 -0300

    Adiciona swagger para documentarn api

--- src/main/java/br/com/nogueira/cooperativismo/CooperativismoApplication.java
+++ src/main/java/br/com/nogueira/cooperativismo/CooperativismoApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableScheduling
+@EnableSwagger2
 public class CooperativismoApplication {
 
 	public static void main(String[] args) {
