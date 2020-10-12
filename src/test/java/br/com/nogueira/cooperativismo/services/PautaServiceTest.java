2f9f94ec079061c3c7da12f0ea20285b6cfba37a
Mateus Nogueira <mateus_nogol@outlook.com>
2020-10-11 21:27:04 -0300

    Adiciona teste para servico de apurar pauta

--- src/test/java/br/com/nogueira/cooperativismo/services/PautaServiceTest.java
+++ src/test/java/br/com/nogueira/cooperativismo/services/PautaServiceTest.java
@@ -1,7 +1,13 @@
 package br.com.nogueira.cooperativismo.services;
 
+import br.com.nogueira.cooperativismo.dtos.ResultadoDto;
 import br.com.nogueira.cooperativismo.entities.Associado;
 import br.com.nogueira.cooperativismo.entities.Pauta;
+import br.com.nogueira.cooperativismo.entities.Sessao;
+import br.com.nogueira.cooperativismo.entities.Voto;
+import br.com.nogueira.cooperativismo.enums.ResultadoEnum;
+import br.com.nogueira.cooperativismo.enums.StatusEnum;
+import br.com.nogueira.cooperativismo.enums.VotoEnum;
 import br.com.nogueira.cooperativismo.exceptions.NotFoundException;
 import br.com.nogueira.cooperativismo.repository.PautaRepository;
 import org.junit.jupiter.api.Test;
@@ -76,6 +82,77 @@ public class PautaServiceTest {
         assertEquals(1,pautas.size());
 
         verify(pautaRepository,times(1)).findByResultadoIsNullAndSessaoDataHoraFinalizacaoBefore(any(LocalDateTime.class));
+    }
+
+    @Test
+    void testaApurarResultadoComEmpate(){
+        Pauta pauta = getPauta();
+        pauta.getSessao().getVotos().add(getVoto(VotoEnum.SIM));
+        pauta.getSessao().getVotos().add(getVoto(VotoEnum.NAO));
+
+        ResultadoDto resultadoDto = pautaService.apurarResultado(pauta);
+
+        assertEquals(1, resultadoDto.getVotosSim());
+        assertEquals(1, resultadoDto.getVotosNao());
+        assertEquals(ResultadoEnum.EMPATADO, resultadoDto.getResultado());
+    }
+
+    @Test
+    void testaApurarResultadoComAprovado(){
+        Pauta pauta = getPauta();
+        pauta.getSessao().getVotos().add(getVoto(VotoEnum.SIM));
+
+        ResultadoDto resultadoDto = pautaService.apurarResultado(pauta);
+
+        assertEquals(1, resultadoDto.getVotosSim());
+        assertEquals(0, resultadoDto.getVotosNao());
+        assertEquals(ResultadoEnum.APROVADO, resultadoDto.getResultado());
+    }
+
+    @Test
+    void testaApurarResultadoComReprovado(){
+        Pauta pauta = getPauta();
+        pauta.getSessao().getVotos().add(getVoto(VotoEnum.NAO));
+
+        ResultadoDto resultadoDto = pautaService.apurarResultado(pauta);
+
+        assertEquals(0, resultadoDto.getVotosSim());
+        assertEquals(1, resultadoDto.getVotosNao());
+        assertEquals(ResultadoEnum.REPROVADO, resultadoDto.getResultado());
+    }
+
+    @Test
+    void testaApurarResultadoSemVotos(){
+        Pauta pauta = getPauta();
+
+        ResultadoDto resultadoDto = pautaService.apurarResultado(pauta);
+
+        assertEquals(0, resultadoDto.getVotosSim());
+        assertEquals(0, resultadoDto.getVotosNao());
+        assertEquals(ResultadoEnum.EMPATADO, resultadoDto.getResultado());
+    }
+
+    private Pauta getPauta(){
+        Pauta pauta = new Pauta();
+
+        Sessao sessao = new Sessao();
+        sessao.setDataHoraFinalizacao(sessao.getDataHoraCriacao().plusSeconds(1));
+
+        pauta.setTitulo("Titulo da pauta");
+        pauta.setDescricao("Descricao da pauta");
+        pauta.setSessao(sessao);
+
+        return pauta;
+    }
+
+    private Voto getVoto(VotoEnum votoEnum){
+        Voto voto = new Voto();
+
+        voto.setVoto(votoEnum);
+        voto.setDataHoraVotacao(LocalDateTime.now());
+        voto.setAssociado(new Associado());
+
+        return voto;
     }
 
 }
